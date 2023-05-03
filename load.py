1945e38981d189f8504dd33e9c00dde3439f941d
Alexis Mendoza <alexisme@nmsu.edu>
2023-05-03 00:21:14 -0600

    V2

--- load.py
+++ load.py
@@ -20,11 +20,12 @@
 import pandas as pd
 import os
 import numpy as np
+import shutil
 
 def generate_payments():
     
     # Define the folder paths for the trucker and broker files
-    trucker_folder_path = './trucker'
+    trucker_folder_path = './truckerv2'
     broker_folder_path = './broker'
 
     # Define the names of the columns in the excel files
@@ -41,32 +42,66 @@ def generate_payments():
     trucker_total_amount_col = 'TOTAL'
    
 
-    # Load all the trucker and broker files into dataframes
-    trucker_files = []
-    for filename in os.listdir(trucker_folder_path):
-        trucker_file = pd.read_excel(os.path.join(trucker_folder_path, filename), usecols=[trucker_load_number_col, trucker_date_col, trucker_total_amount_col])
+    root_folder ='./truckerv2'
+
+    #changing headers
+    #Ensure that all the files have the same headers to join togheter to ensure 
+    #not data corruption
     
-        trucker_files.append(trucker_file)
-    trucker_df = pd.concat(trucker_files) #Creating here a bigass excel file 
+    new_headers = ['CUSTOMER', 'DATE', 'BOL#', 'ORIGIN', 'ORIGIN TICKET #', 'ORIGIN WEIGHT', 'DESTINATION', 'DESTINATION TICKET #', 'DESTINATION WEIGHT', 'BUSHEL / TONS', 'FREIGH RATE', 'TOTAL']
+
+    # loop through all the subfolders and files in the root folder
+    for root, dirs, files in os.walk(root_folder):
+        for file in files:
+         # check if the file is an Excel file
+            if file.endswith('.xlsx') or file.endswith('.xls'):
+            # construct the full file path
+                file_path = os.path.join(root, file)
+            # read the Excel file into a DataFrame
+                df = pd.read_excel(file_path, header=None)
+            # replace the first row with the new column headers
+                df.iloc[0] = new_headers
+            # save the modified DataFrame back to the Excel file
+                with pd.ExcelWriter(file_path) as writer:
+                    df.to_excel(writer, index=False, header=False)
+    
+    #End changing headers
+    
+    data= []
+    for root, dirs, files in os.walk(root_folder):
+        for file in files:
+            # check if the file is an Excel file
+            if file.endswith('.xlsx') or file.endswith('.xls'):
+                # construct the full file path
+                file_path = os.path.join(root, file)
+                # read the Excel file and append its data, directory path, and file name to the list
+                df = pd.read_excel(file_path)
+                df['directory'] = root  # add a new column for the directory path
+                df['filename'] = file  # add a new column for the file name
+                data.append(df)
+
+   
+    trucker_df = pd.concat(data, ignore_index=True)   
+    trucker_df.to_excel("preview.xlsx")
 
     broker_files = []
+    
     for filename in os.listdir(broker_folder_path):
         broker_file = pd.read_excel(os.path.join(broker_folder_path, filename), usecols=[broker_load_number_col, broker_total_amount_col, broker_date_col])
         broker_files.append(broker_file)
     broker_df = pd.concat(broker_files) #Creating here a bigass excel file 
 
-    #print(trucker_df)
 
     trucker_df = trucker_df.rename(columns={trucker_load_number_col: 'Load_Number', trucker_date_col: 'Date_trucker', trucker_total_amount_col: 'Trucker_Amount'})
     broker_df = broker_df.rename(columns={broker_load_number_col: 'Load_Number', broker_total_amount_col: 'Broker_Amount' , broker_date_col: 'Date_broker'})
 
-    #print(trucker_df)
-    #OG RESULT result = pd.merge(trucker_df , broker_df , on ='Load_Number', how ='inner')
+    #Checking if the data matches what the broker says 
     result = pd.merge(trucker_df , broker_df , on ='Load_Number', how ='inner')
     result['match'] = (abs(result['Trucker_Amount'] - result['Broker_Amount']) <= 2)
     result['result'] = np.where(result['match'] == True, 'MATCH', 'Discrepancy')
 
 
+    #Creating the complete and not completed payment loads
     mask = trucker_df['Load_Number'].isin(broker_df['Load_Number'])
                               
     result3 = trucker_df[~mask]
@@ -76,20 +111,45 @@ def generate_payments():
     result3 = result3[mask2]
               
 
-    print("LOADS THAT ALREADY PAID")
+    print("LOADS THAT ALREADY PAID ")
     print("*******************************************")
     print(result)
     result.to_excel('Payments completed.xlsx')
     result3.to_excel('Payments NOT completed.xlsx')
     
+
+# U _____ u _   _    ____                                                        
+# \| ___"|/| \ |"|  |  _"\                                                       
+#  |  _|" <|  \| |>/| | | |                                                      
+#  | |___ U| |\  |uU| |_| |\                                                     
+#  |_____| |_| \_|  |____/ u                                                     
+#  <<   >> ||   \\,-.|||_                                                        
+# (__) (__)(_")  (_/(__)_)                                                       
+#    ____  U _____ u _   _   U _____ u   ____        _       _____  U _____ u    
+# U /"___|u\| ___"|/| \ |"|  \| ___"|/U |  _"\ u U  /"\  u  |_ " _| \| ___"|/    
+# \| |  _ / |  _|" <|  \| |>  |  _|"   \| |_) |/  \/ _ \/     | |    |  _|"      
+#  | |_| |  | |___ U| |\  |u  | |___    |  _ <    / ___ \    /| |\   | |___      
+#   \____|  |_____| |_| \_|   |_____|   |_| \_\  /_/   \_\  u |_|U   |_____|     
+#   _)(|_   <<   >> ||   \\,-.<<   >>   //   \\_  \\    >>  _// \\_  <<   >>     
+#  (__)__) (__) (__)(_")  (_/(__) (__) (__)  (__)(__)  (__)(__) (__)(__) (__)    
+#   ____       _      __   __  __  __  U _____ u _   _     _____   ____          
+# U|  _"\ uU  /"\  u  \ \ / /U|' \/ '|u\| ___"|/| \ |"|   |_ " _| / __"| u       
+# \| |_) |/ \/ _ \/    \ V / \| |\/| |/ |  _|" <|  \| |>    | |  <\___ \/        
+#  |  __/   / ___ \   U_|"|_u | |  | |  | |___ U| |\  |u   /| |\  u___) |        
+#  |_|     /_/   \_\    |_|   |_|  |_|  |_____| |_| \_|   u |_|U  |____/>>       
+#  ||>>_    \\    >>.-,//|(_ <<,-,,-.   <<   >> ||   \\,-._// \\_  )(  (__)      
+# (__)__)  (__)  (__)\_) (__) (./  \.) (__) (__)(_")  (_/(__) (__)(__)   
     
+    
+################################################################################################
     
 
 #This function will create a duplicate of trucker folder with the same files but the data inside the file will be 
 #Only the loads paid from that file
+
 def duplicates_with_only_payed_loads():
     # Define the folder paths for the trucker and broker files
-    trucker_folder_path = './trucker'
+    trucker_folder_path = './truckerv2'
     broker_folder_path = './broker'
     trucker_files_paid_only = './trucker_only_paid_loads'
     trucker_files_not_paid = './truckers_only_not_paid_loads'
@@ -109,16 +169,30 @@ def duplicates_with_only_payed_loads():
     
      #############################################
 
-    # Load all the trucker and broker files into dataframes
-    trucker_files = []
     entire_trucker_files = [] 
     
-    for filename in os.listdir(trucker_folder_path):
-        trucker_file = pd.read_excel(os.path.join(trucker_folder_path, filename), usecols=[trucker_load_number_col, trucker_date_col, trucker_total_amount_col])
-        entire_trucker_file = pd.read_excel(os.path.join(trucker_folder_path, filename)) #This Line stores the entire row of the excel file 
-        trucker_files.append(trucker_file)
-        entire_trucker_files.append((entire_trucker_file,filename))
-    trucker_df = pd.concat(trucker_files) #Creating here a bigass excel file 
+
+    data= []
+    for root, dirs, files in os.walk(trucker_folder_path):
+        for file in files:
+                # check if the file is an Excel file
+            if file.endswith('.xlsx') or file.endswith('.xls'):
+                    # construct the full file path
+                file_path = os.path.join(root, file)
+                    # read the Excel file and append its data, directory path, and file name to the list
+                df = pd.read_excel(file_path)
+                df['directory'] = root  # add a new column for the directory path
+                df['filename'] = file  # add a new column for the file name
+                data.append(df)
+
+   
+    trucker_df = pd.concat(data, ignore_index=True)   
+    trucker_df['directory'] = trucker_df['directory'].str.replace("./truckerv2/", "./load_payed/")
+   
+   #Debbuging code 
+   # trucker_df.to_excel("preview2ndfile.xlsx")
+
+
 
     broker_files = []
     for filename in os.listdir(broker_folder_path):
@@ -126,58 +200,147 @@ def duplicates_with_only_payed_loads():
         broker_files.append(broker_file)
     broker_df = pd.concat(broker_files) #Creating here a bigass excel file 
 
+    broker_df.to_excel("2ndbrokerdf.xlsx")
     
     trucker_df = trucker_df.rename(columns={trucker_load_number_col: 'Load_Number', trucker_date_col: 'Date_Trucker', trucker_total_amount_col: 'Trucker_Amount'})
     broker_df = broker_df.rename(columns={broker_load_number_col: 'Load_Number', broker_total_amount_col: 'Broker_Amount', broker_date_col: 'Date_Broker'})
 
     #print(trucker_df)
-    result = pd.merge(trucker_df , broker_df , on ='Load_Number', how ='inner')
+    result = pd.merge(trucker_df , broker_df , on ='Load_Number')
+    result.to_excel("whatdata.xlsx")
+    
+    result['match'] = (abs(result['Trucker_Amount'] - result['Broker_Amount']) <= 2)
+    result['result'] = np.where(result['match'] == True, 'MATCH', 'Discrepancy')
+    
+    grouped_df = result.groupby(['directory','filename'])
+    
+    #Deletes the path to dont overlay the output
+    if os.path.isdir("./load_payed"):
+        shutil.rmtree("./load_payed")
+        
+    #The following code writes the data into the excel files 
+    #and stores in the corresponging folder instead of just by name
+    #The completed payments
+        
+    for group, data in grouped_df:
+        directory, filename = group
+        output_path = directory + '/'
+        output_file = directory + '/' + filename
+        
+        # Create the directory if it doesn't exist
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        
+        if os.path.isfile(output_file):
+            # Load the existing Excel file
+            existing_df = pd.read_excel(output_file)
 
+            # Concatenate the existing data with the new data
+            combined_df = pd.concat([existing_df, data], axis=0, ignore_index=True)
+
+            # Save the combined data to the file
+            combined_df.to_excel(output_file, index=False)
+        else:
+            # Save the new data to a new Excel file at the specified output path
+            data.to_excel(output_file, index=False)
+    
+    #Incompleted payments
+    
+     #Deletes the path to dont overlay the output
+    if os.path.isdir("./load_not_payed"):
+        shutil.rmtree("./load_not_payed")
+     
+    not_completed_df = pd.read_excel("./Payments NOT completed.xlsx")
+    
+    not_completed_df['directory'] = not_completed_df['directory'].str.replace("./truckerv2/", "./load_not_payed/")
+        
+    not_grouped_df = not_completed_df.groupby(['directory','filename'])
+    
+    for group, data in not_grouped_df:
+        directory, filename = group
+        output_path = directory + '/'
+        output_file = directory + '/' + filename
+        
+        # Create the directory if it doesn't exist
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        
+        if os.path.isfile(output_file):
+            # Load the existing Excel file
+            existing_df = pd.read_excel(output_file)
+
+            # Concatenate the existing data with the new data
+            combined_df = pd.concat([existing_df, data], axis=0, ignore_index=True)
+
+            # Save the combined data to the file
+            combined_df.to_excel(output_file, index=False)
+        else:
+            # Save the new data to a new Excel file at the specified output path
+            data.to_excel(output_file, index=False)
+    
+    
+    #Legacy Code
+    #Relevant for the implementation of the ammenment payments functioanlity 
+    
+    ## Shows duplicate payments 
     local = result
     temporal = result.duplicated(subset=['Load_Number'], keep = False)
-    
     local = local[temporal]
     local.to_excel('No Dup.xlsx')
-
     temporal.to_excel('Dup.xlsx')
+    #End Duplicate payments 
 
 
 
-    for excel_files,name in entire_trucker_files:
-        excel_files = excel_files.rename(columns={trucker_load_number_col: 'Load_Number'})
-        
-        
-        
-        result2 = pd.merge(excel_files , result , on ='Load_Number', how ='inner')
-        
-        mask = excel_files['Load_Number'].isin(result['Load_Number'])
-        
-        result3 = excel_files[~mask]
-        
-        
-        
-        mask2 = result3['Load_Number'].notnull()
-    
-        result3 = result3[mask2]
-        
-        
-        result2['match'] = (abs(result2['Trucker_Amount'] - result2['Broker_Amount']) <= 2)
-        result2['result'] = np.where(result2['match'] == True, 'MATCH', 'Discrepancy')
+#  _______  _        ______                                                                 
+# (  ____ \( (    /|(  __  \                                                                
+# | (    \/|  \  ( || (  \  )                                                               
+# | (__    |   \ | || |   ) |                                                               
+# |  __)   | (\ \) || |   | |                                                               
+# | (      | | \   || |   ) |                                                               
+# | (____/\| )  \  || (__/  )                                                               
+# (_______/|/    )_)(______/                                                                
+                                                                                          
+#  ______            _______  _       _________ _______  _______ _________ _______  _______ 
+# (  __  \ |\     /|(  ____ )( \      \__   __/(  ____ \(  ___  )\__   __/(  ____ \(  ____ \
+# | (  \  )| )   ( || (    )|| (         ) (   | (    \/| (   ) |   ) (   | (    \/| (    \/
+# | |   ) || |   | || (____)|| |         | |   | |      | (___) |   | |   | (__    | (_____ 
+# | |   | || |   | ||  _____)| |         | |   | |      |  ___  |   | |   |  __)   (_____  )
+# | |   ) || |   | || (      | |         | |   | |      | (   ) |   | |   | (            ) |
+# | (__/  )| (___) || )      | (____/\___) (___| (____/\| )   ( |   | |   | (____/\/\____) |
+# (______/ (_______)|/       (_______/\_______/(_______/|/     \|   )_(   (_______/\_______)
+                                                                                          
+#  _______  _        _                                                                      
+# (  ___  )( (    /|( \   |\     /|                                                         
+# | (   ) ||  \  ( || (   ( \   / )                                                         
+# | |   | ||   \ | || |    \ (_) /                                                          
+# | |   | || (\ \) || |     \   /                                                           
+# | |   | || | \   || |      ) (                                                            
+# | (___) || )  \  || (____/\| |                                                            
+# (_______)|/    )_)(_______/\_/                                                            
+                                                                                          
+#  _______  _______           _______  ______                                               
+# (  ____ )(  ___  )|\     /|(  ____ \(  __  \                                              
+# | (    )|| (   ) |( \   / )| (    \/| (  \  )                                             
+# | (____)|| (___) | \ (_) / | (__    | |   ) |                                             
+# |  _____)|  ___  |  \   /  |  __)   | |   | |                                             
+# | (      | (   ) |   ) (   | (      | |   ) |                                             
+# | )      | )   ( |   | |   | (____/\| (__/  )                                             
+# |/       |/     \|   \_/   (_______/(______/        
 
-        local2 = result3.duplicated(subset=['Load_Number'], keep = False)
-        local2 = result3[local2]
-        local2.to_excel('./test/'+name+'.xlsx')
-                
-        result2.to_excel(trucker_files_paid_only+"/"+name)
-                        #'./trucker_only_paid_loads'/ Filename
-        
-        result3.to_excel(trucker_files_not_paid+"/"+name)
-              
+
+
 
 #NOTA PARA ALEXIS ESTA PARTE DEL CODIGO AHORITA ES ESTE MOMENTO SE ME HACE ETERNA SI EL CODIGO YA FUNCIONA
 # MARZO 31 2023
 
+
 #ALEXIS DEL FUTURO ACABA ESTA PARTE DEL CODIGO ASAP 
+
+
+#NOTA  PARA ALEXIS YA LE AGARRASTE EL PEDO
+#MAYO 2 2023
+
+
+
 
 def fixed_payments():
     # Define the folder paths for the trucker and broker files
@@ -198,24 +361,7 @@ def fixed_payments():
     trucker_total_amount_col = 'TOTAL'
 
 
-    trucker_files=[]
-    entire_trucker_files=[]
-
-    #opening the files in trucker paid loads 
-    for filename in os.listdir(trucker_paided):
-        trucker_file = pd.read_excel(os.path.join(trucker_paided, filename), usecols=[trucker_load_number_col, trucker_date_col, trucker_total_amount_col])
-        entire_trucker_file = pd.read_excel(os.path.join(trucker_paided, filename)) #This Line stores the entire row of the excel file 
-        trucker_files.append(trucker_file)
-        entire_trucker_files.append((entire_trucker_file,filename))
-
-    #Opening the files in claims fixed 
-
-    for filename in os.listdir(claims):
-        claim_file = pd.read_excel()
-
-    
-
-
+    #Deleted because overhaul of the entire code 
 
 
 #Calling the programs 
